f73e14673736fe517019ec8c305ca0ad78d1c46d
Morgan Bradford <morganjbradford95@gmail.com>
2021-02-05 13:02:27 -0600

    refactor tests toFixed(1). add test 8; good fail

--- __tests__/sgaCalc.test.js
+++ __tests__/sgaCalc.test.js
@@ -12,26 +12,30 @@ test('should correctly create an instance of SuperGalacticAgeCalculator', () => 
   });
 
 test('should correctly return this.age * 0.24', () => {
-    expect(calc.mercuryYears()).toEqual(5.28);
+    expect(calc.mercuryYears()).toEqual(5.3);
   });
 
 test('should correctly return this.age * 0.62', () => {
-    expect(calc.venusYears()).toEqual(13.64);
+    expect(calc.venusYears()).toEqual(13.6);
   });
 
 test('should correctly return this.age * 1.88', () => {
-    expect(calc.marsYears()).toEqual(41.36);
+    expect(calc.marsYears()).toEqual(41.4);
   });
 
 test('should correctly return this.age * 11.86', () => {
-    expect(calc.jupiterYears()).toEqual(260.92);
+    expect(calc.jupiterYears()).toEqual(260.9);
   });
 
-  test('should correctly return life expectancy for this.age in Earth years', () => {
+test('should correctly return life expectancy for this.age in Earth years', () => {
     expect(calc.lifeExpectancy()).toEqual(56.8);
   });
 
   test('should correctly return life expectancy for this.age in Mercury years', () => {
     expect(calc.mercuryLifeExpectancy()).toEqual(13.6);
+  });
+
+test('should correctly return life expectancy for this.age in Venus years', () => {
+    expect(calc.venusLifeExpectancy()).toEqual(35.3);
   });
 });
