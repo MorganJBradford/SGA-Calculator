f7efced3e672553e51c8ba7b67db48d0968c3345
Morgan Bradford <morganjbradford95@gmail.com>
2021-02-05 10:46:49 -0600

    refactor 1st red test; good fail

--- __tests__/sgaCalc.test.js
+++ __tests__/sgaCalc.test.js
@@ -1,8 +1,9 @@
-import { superGalacticAgeCalculator } from "../src/js/sgaCalc";
+import { SuperGalacticAgeCalculator } from "../src/js/sgaCalc";
 
-describe('superGalacticAgeCalculator', () => {
+describe('SuperGalacticAgeCalculator', () => {
 
-  test('should correctly create an instance of superGalacticAgeCalculator', () => {
-    expect(superGalacticAgeCalculator.user).toEqual({});
+  test('should correctly create an instance of SuperGalacticAgeCalculator', () => {
+    let calc = new SuperGalacticAgeCalculator(22);
+    expect(calc.age).toEqual(22);
   });
 });
--- src/js/sgaCalc.js
+++ src/js/sgaCalc.js
@@ -1,5 +1,4 @@
-export default class superGalacticAgeCalculator {
+export class SuperGalacticAgeCalculator {
   constructor(){
-
   }
 };
