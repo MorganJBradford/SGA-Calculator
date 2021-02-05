d2feddb36f0b943a1c816ea084c86287416b8389
Morgan Bradford <morganjbradford95@gmail.com>
2021-02-05 10:29:46 -0600

    sgaCalc 1st good fail

---
+++ __tests__/sgaCalc.test.js
@@ -0,0 +1,8 @@
+import { superGalacticAgeCalculator } from "../src/js/sgaCalc";
+
+describe('superGalacticAgeCalculator', () => {
+
+  test('should correctly create an instance of superGalacticAgeCalculator', () => {
+    expect(superGalacticAgeCalculator.user).toEqual({});
+  });
+});
