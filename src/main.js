c754a2fb247d7a6ac86bc6d0b30ebcd3f747f236
Morgan Bradford <morganjbradford95@gmail.com>
2021-02-05 20:02:53 -0600

    add display Mercury years

--- src/main.js
+++ src/main.js
@@ -4,8 +4,12 @@ import $ from 'jquery';
 // import './css/styles.css';
 // import './css/styles.css';
 import { SuperGalacticAgeCalculator } from './js/sgaCalc.js';
-
+let calc;
 $("#galactic-age").submit(function(event) {
   event.preventDefault();
-  let calc = new SuperGalacticAgeCalculator($('#age').val());
+  calc = new SuperGalacticAgeCalculator($('#age').val());
+});
+$("#mercury-years").on("click", function() {
+  calc.mercuryYears();
+  $('p').text(calc.planetYears);
 });
